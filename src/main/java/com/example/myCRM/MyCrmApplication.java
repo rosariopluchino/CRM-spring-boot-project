f4aaef0d0200d194661ded5e5cdfaf907b47c8ad
rosario.pluchino <rosario.pluchino@aitho.it>
2025-12-04 12:02:19 +0100

    First commit: spring boot CRM project

---
+++ src/main/java/com/example/myCRM/MyCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.myCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrmApplication.class, args);
+	}
+
+}
