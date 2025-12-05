e7bc85d4f0dcf1b6c17874dd6c3fbb0bc48345e8
rosario.pluchino <rosario.pluchino@aitho.it>
2025-12-05 16:55:57 +0100

    feat: output dto

---
+++ src/main/java/com/example/myCRM/dto/ResponseClientModelDTO.java
@@ -0,0 +1,13 @@
+package com.example.myCRM.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseClientModelDTO {
+    Long id;
+    String name;
+    String surname;
+    String email;
+}
