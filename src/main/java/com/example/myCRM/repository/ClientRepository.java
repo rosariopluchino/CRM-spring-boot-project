42b9b5a12d1e43b9e35ab04fdb76e2bf44481a13
rosario.pluchino <rosario.pluchino@aitho.it>
2025-12-19 10:16:14 +0100

    feat: added method getClientById

--- src/main/java/com/example/myCRM/repository/ClientRepository.java
+++ src/main/java/com/example/myCRM/repository/ClientRepository.java
@@ -1,5 +1,7 @@
 package com.example.myCRM.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.myCRM.model.ClientModel;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientModel, Long> {
+    public Optional<ClientModel> getClientById(Long id);
 }
